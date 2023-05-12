3051d07d87bb68be8ec0b3d9c3b017ee2bb943a1
rdmc10 <radu.m10@protonmail.com>
2023-05-12 17:39:13 +0300

    Added Administrator Window and VM

---
+++ mvp3/ViewModel/AdminWindowVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mvp3.Helpers;
+
+namespace mvp3.ViewModel
+{
+    public class AdminWindowVM : BaseVM
+    {
+
+    }
+}
