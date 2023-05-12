64056717a89686950ffbb3a35c5284e2cd4f4355
rdmc10 <radu.m10@protonmail.com>
2023-05-12 23:59:08 +0300

    Administrator can now add students in database

--- mvp3/ViewModel/EditStudentsVM.cs
+++ mvp3/ViewModel/EditStudentsVM.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
 using mvp3.Helpers;
 using mvp3.Model;
 
@@ -15,10 +19,49 @@ namespace mvp3.ViewModel
         private SchoolEntities3 context = new SchoolEntities3();
         public ObservableCollection<USER> Students { get; set; }
 
+        private string _name { get; set; }
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                NotifyPropertyChanged(nameof(Name));
+            }
+        }
         public EditStudentsVM()
         {
             LoadStudents();
         }
+
+        public ICommand CreateUserCommand => new RelayCommand(CreateUser);
+
+        private void CreateUser()
+        {
+            if (Name != null)
+            {
+                string username = Name.Replace(" ", "").ToLower();
+                string password = username + "123";
+                int typeId = 1;
+
+                USER newUser = new USER()
+                {
+                    Name = Name,
+                    Password = password,
+                    Username = username,
+                    UserTypeId = typeId
+                };
+
+                Students.Add(newUser);
+                context.AddUser(Name, username, password, typeId);
+            }
+            else
+            {
+                MessageBox.Show("You can't create a user without a name!");
+            }
+        }
+
+        
 
         public void LoadStudents()
         {
